8f7a3b6d87f4b5fac5dd82f841ece277523186f3
gky360 <gky360@gmail.com>
2020-06-16 20:55:46 +0900

    Misc

--- src/components/Stage/Viewer.tsx
+++ src/components/Stage/Viewer.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { useStage } from '../../hooks';
+// import { useStage } from '../../hooks';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ViewerProps {}
 
 export const Viewer: React.FC<ViewerProps> = ({ children }) => {
-  const stage = useStage();
-  const { viewer } = stage;
-
-  // eslint-disable-next-line no-console
-  console.log(viewer);
+  // const stage = useStage();
+  // const { viewer } = stage;
 
   return <>{children}</>;
 };
