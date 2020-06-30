19b6236f2695c2b27a18212c3610703ed7ce793b
gky360 <gky360@gmail.com>
2020-07-01 04:03:35 +0900

    Add stories

--- src/components/Stage/Stage.stories.tsx
+++ src/components/Stage/Stage.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import throttle from 'lodash/throttle';
-import { CameraState } from '../../utils';
+import { CameraState, Position, Rotation } from '../../utils';
 import { Component } from '../Component/Component';
 import { Stage } from './Stage';
 
@@ -38,8 +38,66 @@ export const CaptureCameraState = () => {
       <Stage width="600px" height="400px" onCameraMove={handleCameraMove}>
         <Component path="rcsb://4hhb" reprList={reprList} />
       </Stage>
+
+      <h4>Camera state</h4>
       <pre>
-        {cameraState ? JSON.stringify(cameraState, undefined, 2) : 'N/A'}
+        {cameraState ? JSON.stringify(cameraState, undefined, 2) : 'Default'}
+      </pre>
+    </>
+  );
+};
+
+export const ControllCameraState = () => {
+  const THROTTLE_MS = 100;
+  const cameraStateA = {
+    position: new Position(7, 28, -3),
+    rotation: new Rotation(0.8, 0.4, 0, -0.5),
+    distance: -60,
+  };
+  const cameraStateB: CameraState = {
+    position: new Position(-25, 145, 150),
+    rotation: new Rotation(-0.6, 0.1, 0.5, 0.7),
+    distance: -500,
+  };
+  const reprList = useMemo(() => [{ type: 'cartoon' as const }], []);
+
+  const [cameraState, setCameraState] = useState<CameraState>();
+
+  const handleCameraMove = useMemo(
+    () =>
+      throttle(
+        (nextCameraState: CameraState) => setCameraState(nextCameraState),
+        THROTTLE_MS
+      ),
+    []
+  );
+
+  return (
+    <>
+      <Stage
+        width="600px"
+        height="400px"
+        cameraState={cameraState}
+        onCameraMove={handleCameraMove}
+      >
+        <Component path="rcsb://4hhb" reprList={reprList} />
+      </Stage>
+
+      <div>
+        <button type="button" onClick={() => setCameraState(cameraStateA)}>
+          State A
+        </button>
+        <button type="button" onClick={() => setCameraState(cameraStateB)}>
+          State B
+        </button>
+        <button type="button" onClick={() => setCameraState(undefined)}>
+          Reset
+        </button>
+      </div>
+
+      <h4>Camera state</h4>
+      <pre>
+        {cameraState ? JSON.stringify(cameraState, undefined, 2) : 'Default'}
       </pre>
     </>
   );
