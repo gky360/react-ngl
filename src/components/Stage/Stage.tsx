164d1f6ab71e0f27903fd665e1cc6199fe02f029
gky360 <gky360@gmail.com>
2020-07-01 02:04:17 +0900

    Add camera move handler

--- src/components/Stage/Stage.tsx
+++ src/components/Stage/Stage.tsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState, RefCallback, useCallback } from 'react';
 import { StageReactContext } from '../../hooks';
-import { NGL } from '../../utils';
+import { NGL, CameraState, getCameraState } from '../../utils';
 import { Viewer } from './Viewer';
 
 export interface StageProps {
-  params?: Partial<NGL.StageParameters>;
   width: string;
   height: string;
+  params?: Partial<NGL.StageParameters>;
+  onCameraMove?: (cameraState: CameraState) => void;
 }
 
 export const Stage: React.FC<StageProps> = ({
   children,
-  params: parameters,
   width,
   height,
+  params,
+  onCameraMove,
 }) => {
   const [stage, setStage] = useState<NGL.Stage>();
 
@@ -34,15 +36,33 @@ export const Stage: React.FC<StageProps> = ({
 
   useEffect(() => {
     if (stage) {
-      stage.setParameters(parameters);
+      stage.setParameters(params);
     }
-  }, [parameters, stage]);
+  }, [params, stage]);
 
   useEffect(() => {
     if (stage) {
       stage.setSize(width, height);
     }
   }, [height, stage, width]);
+
+  const handleCameraMove = useCallback(() => {
+    if (stage && onCameraMove) {
+      const cameraState = getCameraState(stage);
+      onCameraMove(cameraState);
+    }
+  }, [onCameraMove, stage]);
+
+  useEffect(() => {
+    if (stage) {
+      stage.viewerControls.signals.changed.add(handleCameraMove);
+    }
+    return (): void => {
+      if (stage) {
+        stage.viewerControls.signals.changed.remove(handleCameraMove);
+      }
+    };
+  }, [handleCameraMove, stage]);
 
   return (
     <>
