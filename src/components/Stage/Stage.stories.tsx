1f16860ae62010463987666bb3450e037d929221
gky360 <gky360@gmail.com>
2020-07-01 02:19:03 +0900

    Add stories

--- src/components/Stage/Stage.stories.tsx
+++ src/components/Stage/Stage.stories.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import throttle from 'lodash/throttle';
+import { CameraState } from '../../utils';
+import { Component } from '../Component/Component';
 import { Stage } from './Stage';
 
 export default {
@@ -6,4 +9,38 @@ export default {
   component: Stage,
 };
 
-export const Empty = () => <Stage width="600px" height="400px" />;
+export const Usage = () => {
+  const reprList = useMemo(() => [{ type: 'cartoon' as const }], []);
+  return (
+    <Stage width="600px" height="400px">
+      <Component path="rcsb://4hhb" reprList={reprList} />
+    </Stage>
+  );
+};
+
+export const CaptureCameraState = () => {
+  const THROTTLE_MS = 100;
+  const reprList = useMemo(() => [{ type: 'cartoon' as const }], []);
+
+  const [cameraState, setCameraState] = useState<CameraState>();
+
+  const handleCameraMove = useMemo(
+    () =>
+      throttle(
+        (nextCameraState: CameraState) => setCameraState(nextCameraState),
+        THROTTLE_MS
+      ),
+    []
+  );
+
+  return (
+    <>
+      <Stage width="600px" height="400px" onCameraMove={handleCameraMove}>
+        <Component path="rcsb://4hhb" reprList={reprList} />
+      </Stage>
+      <pre>
+        {cameraState ? JSON.stringify(cameraState, undefined, 2) : 'N/A'}
+      </pre>
+    </>
+  );
+};
