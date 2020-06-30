a4b368c491f9f83db5d11f3aea42a3c80f732cae
gky360 <gky360@gmail.com>
2020-07-01 01:28:55 +0900

    Revert "Fix representation replacement"
    This reverts commit 5c95e4f8204f1ba351f7513c909d7355d7fa410c.

--- src/components/Component/Component.tsx
+++ src/components/Component/Component.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
-import isEqual from 'lodash/isEqual';
+import React, { useEffect, useState } from 'react';
 import useAsyncEffect from '@n1ru4l/use-async-effect';
 import { NGL } from '../../utils';
 import { useStage } from '../../hooks';
@@ -12,20 +11,15 @@ export interface ComponentProps {
   onLoadFailure?: (error: Error) => void;
 }
 
-const defaultReprList: NGL.RepresentationDescriptor[] = [];
-
 export const Component: React.FC<ComponentProps> = ({
   path,
   loadFileParams,
   params,
-  reprList = defaultReprList,
+  reprList = [],
   onLoadFailure,
 }) => {
-  const prevReprListRef = useRef<NGL.RepresentationDescriptor[]>(
-    defaultReprList
-  );
-  const [component, setComponent] = useState<NGL.Component>();
   const stage = useStage();
+  const [component, setComponent] = useState<NGL.Component>();
 
   useAsyncEffect(function* loadFiles(setCancelHandler, c) {
     const abortController = new AbortController();
@@ -48,20 +42,10 @@ export const Component: React.FC<ComponentProps> = ({
 
   useEffect(() => {
     if (component) {
-      const prevReprList = prevReprListRef.current;
-      const len = Math.max(prevReprList.length, reprList.length);
-      for (let i = 0; i < len; i += 1) {
-        if (!isEqual(prevReprList[i], reprList[i])) {
-          const prevReprElem = component.reprList[i];
-          if (prevReprElem) {
-            component.removeRepresentation(prevReprElem);
-          }
-          if (reprList[i]) {
-            component.addRepresentation(reprList[i].type, reprList[i].params);
-          }
-        }
-      }
-      prevReprListRef.current = reprList;
+      component.removeAllRepresentations();
+      reprList.forEach((repr) =>
+        component.addRepresentation(repr.type, repr.params)
+      );
     }
   }, [component, reprList]);
 
