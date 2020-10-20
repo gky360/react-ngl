fddae925059deb92b5870a8c3e17019085812a19
gky360 <gky360@gmail.com>
2020-10-20 22:25:27 +0900

    Fix type imports

--- src/utils/ngl/nglTypes.ts
+++ src/utils/ngl/nglTypes.ts
@@ -1,19 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-export * from '../../@types/ngl/declarations/ngl';
-// export {
-// Matrix3,
-// Matrix4,
-// Vector2,
-// Vector3,
-// Box3,
-// Quaternion,
-// Euler,
-// Plane,
-// Color,
-// } from 'three';
+export * from 'ngl';
+export {
+  Matrix3,
+  Matrix4,
+  Vector2,
+  Vector3,
+  Box3,
+  Quaternion,
+  Euler,
+  Plane,
+  Color,
+} from 'three';
 export type { StageParameters } from '../../@types/ngl/declarations/stage/stage';
 export type { ComponentParameters } from '../../@types/ngl/declarations/component/component';
-// export type { StructureRepresentationType } from '../../@types/ngl/declarations/component/structure-component';
+export type { StructureRepresentationType } from '../../@types/ngl/declarations/component/structure-component';
 export type { SurfaceRepresentationType } from '../../@types/ngl/declarations/component/surface-component';
 export type { StageLoadFileParams } from '../../@types/ngl/declarations/stage/stage';
